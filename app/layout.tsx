062e3df8afd9cb9962d88367a5883f2f193ac388
Samay Shadani <shadanisamay@gmail.com>
2025-07-04 15:33:59 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,33 @@ export default function RootLayout({
             `
           }}
         />
+          <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1528358318152104');
+        fbq('track', 'PageView');
+      `
+    }}
+  />
+
+  <noscript>
+    <img
+      height="1"
+      width="1"
+      style={{ display: 'none' }}
+      src="https://www.facebook.com/tr?id=1528358318152104&ev=PageView&noscript=1"
+      alt=""
+    />
+  </noscript>
+
       </head>
       <body className={inter.className}> <Navbar /> {children} <Footer />
         <WhatsAppButton />
